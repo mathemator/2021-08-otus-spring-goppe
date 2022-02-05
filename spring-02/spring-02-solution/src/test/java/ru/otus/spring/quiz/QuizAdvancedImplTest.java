bb39f31e5f11cd6ac2d5b96234bba42d7d1ae0ad
mathemator <mathemator@yandex.ru>
2022-02-05 12:53:03 +0300

    ДЗ 2: промежуточный коммит-слияние с рефакторингом по рекомендациям из урока: тест

--- spring-02/spring-02-solution/src/test/java/ru/otus/spring/quiz/QuizAdvancedImplTest.java
+++ spring-02/spring-02-solution/src/test/java/ru/otus/spring/quiz/QuizAdvancedImplTest.java
@@ -3,6 +3,8 @@ package ru.otus.spring.quiz;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import ru.otus.spring.io.IOService;
+import ru.otus.spring.message.MessageProvider;
 import ru.otus.spring.quiz.advanced.QuizAdvancedImpl;
 import ru.otus.spring.quiz.question.Question;
 import ru.otus.spring.quiz.question.QuestionParser;
@@ -18,10 +20,15 @@ public class QuizAdvancedImplTest {
 
     private QuizAdvancedImpl quizAdvanced;
     private QuestionParser questionParserMock;
+    private IOService ioServiceMock;
+    private MessageProvider messageProviderMock;
+
 
     @Before
     public void setUp() {
-
+        ioServiceMock = Mockito.mock(IOService.class);
+        Mockito.when(ioServiceMock.readString()).thenReturn("1");
+        messageProviderMock = Mockito.mock(MessageProvider.class);
         questionParserMock = Mockito.mock(QuestionParser.class);
         Mockito.when(questionParserMock.fromLine(Mockito.any())).thenReturn(
                 new Question("question?", Arrays.asList("answer1", "answer2"), 1));
@@ -29,32 +36,16 @@ public class QuizAdvancedImplTest {
     }
 
     @Test
-    public void testSuccessBehaviour() throws IOException {
-        quizAdvanced = new QuizAdvancedImpl(questionParserMock, "/test.csv", 3);
-
-        String input = "1\n" +
-                "1\n" +
-                "1\n" +
-                "1\n" +
-                "1\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+    public void testSuccessBehaviour() {
+        quizAdvanced = new QuizAdvancedImpl(ioServiceMock, questionParserMock, "/test.csv", messageProviderMock, 3);
 
         PassageStatus display = quizAdvanced.display();
         assertEquals(PassageStatus.SUCCESS, display, "result must be success");
     }
 
     @Test
-    public void testFailBehaviour() throws IOException {
-        quizAdvanced = new QuizAdvancedImpl(questionParserMock, "/test.csv", 6);
-
-        String input = "1\n" +
-                "1\n" +
-                "1\n" +
-                "1\n" +
-                "1\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+    public void testFailBehaviour() {
+        quizAdvanced = new QuizAdvancedImpl(ioServiceMock, questionParserMock, "/test.csv", messageProviderMock, 6);
 
         PassageStatus display = quizAdvanced.display();
         assertEquals(PassageStatus.FAILED, display, "result must be failed");
