d350be4f841dd288dce204d1b44e84cf64d57b1d
mathemator <mathemator@yandex.ru>
2022-02-13 07:59:16 +0300

    ДЗ spring boot - исправления messageProvider

--- spring-03-spring-boot/src/main/java/ru/otus/spring/message/MessageProvider.java
+++ spring-03-spring-boot/src/main/java/ru/otus/spring/message/MessageProvider.java
@@ -1,6 +1,5 @@
 package ru.otus.spring.message;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
@@ -10,11 +9,13 @@ import java.util.Locale;
 @Component
 public class MessageProvider {
 
-    @Value("${locale}")
-    String locale;
+    private final String locale;
+    private final MessageSource messageSource;
 
-    @Autowired
-    MessageSource messageSource;
+    public MessageProvider(@Value("${locale}") String locale, MessageSource messageSource) {
+        this.locale = locale;
+        this.messageSource = messageSource;
+    }
 
     public String getMessage(String placeholder) {
         return messageSource.getMessage(
